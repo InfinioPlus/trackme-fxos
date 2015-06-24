59ac01695406c345dc5909c3b92a1f429bd2310c
Harsh Vardhan <harshvd95@gmail.com>
2015-06-24 20:57:10 +0530

    Adding AutoComplete Search feature

--- js/project.js
+++ js/project.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    var map = null;
     
     var mapOptions = {
         center: new google.maps.LatLng(13.685449, -89.239938),
@@ -7,5 +6,35 @@ $(document).ready(function(){
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     
-	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+	
+	//AutoComplete
+	var acOptions = {
+		types: ['establishment']
+	};
+	var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
+	autocomplete.bindTo('bounds',map);
+	var infoWindow = new google.maps.InfoWindow();
+	var marker = new google.maps.Marker({
+	  map: map
+	});
+
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+	  infoWindow.close();
+	  var place = autocomplete.getPlace();
+	  if (place.geometry.viewport) {
+		map.fitBounds(place.geometry.viewport);
+	  } else {
+		map.setCenter(place.geometry.location);
+		map.setZoom(17);
+	  }
+	  marker.setPosition(place.geometry.location);
+	  infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+	  infoWindow.open(map, marker);
+	  google.maps.event.addListener(marker,'click',function(e){
+
+		infoWindow.open(map, marker);
+
+	  });
+	});
 });
