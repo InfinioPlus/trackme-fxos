0ea4ddda3c9521d20fdab3a0ea81bb95cd33f21b
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-06-29 02:37:12 +0200

    Basic geolocation watcher

--- js/project.js
+++ js/project.js
@@ -10,9 +10,44 @@ $(document).ready(function(){
 	
 	
     
+    var continue_sending = true;
+    var first_response = true;
+    var watch_id;
+    var lat;
+    var lng;
+    
     $('#start-btn').click(function(){
+        startConn();
+        trackPosition();
     });
     
     $('#stop-btn').click(function(){
     });
+    
+    
+    function startConn(){
+        $.get("start_conn.php", function(data, status){
+            // as output data I need:
+            // 1. a GUID sent by the server
+            // 2. as output by the PHP script,  
+        });
+    }
+    
+    function trackPosition(){
+        var options = {
+            timeout: 60000
+        };
+        
+        watch_id = navigator.geolocation.watchPosition(onPositionChange, onPositionError, options);
+    }
+    
+    function onPositionChange(pos){
+        var coords = pos.coords;
+        lat = coords.latitude;
+        lng = coords.longitude;
+    }
+    
+    function onPositionError(err){
+        console.log("Error on watcher: " + err.message);
+    }
 });
