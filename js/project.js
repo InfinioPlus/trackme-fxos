e93121110613e935011a287901af0348743ece8c
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-06-29 02:11:54 +0200

    Added basic listeners

--- js/project.js
+++ js/project.js
@@ -9,4 +9,10 @@ $(document).ready(function(){
 	var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 	
 	
+    
+    $('#start-btn').click(function(){
+    });
+    
+    $('#stop-btn').click(function(){
+    });
 });
